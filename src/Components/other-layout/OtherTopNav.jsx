3e9300ddc848ae1d249a6494179cc4ce930a5873
kareem roqib <kareemolakunleh@gmail.com>
2024-08-07 06:21:19 +0100

    linking the neccessary pages

--- src/Components/other-layout/OtherTopNav.jsx
+++ src/Components/other-layout/OtherTopNav.jsx
@@ -1,8 +1,12 @@
 import { FaHeart, FaLongArrowAltLeft, FaRegHeart } from "react-icons/fa";
 import { TiGroupOutline } from "react-icons/ti";
 import { LiaTrophySolid } from "react-icons/lia";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { cn } from "@/lib/utils";
 
-const OtherTopNav = () => {
+const OtherTopNav = ({ setShowModal }) => {
+  const handleModal = () => setShowModal((prev) => !prev);
+  const location = useLocation();
   return (
     <nav className="flex gap-[97px] px-3 py-[25px] md:px-5">
       <div className="flex w-full items-center justify-between gap-1 md:gap-6 lg:w-max lg:justify-normal">
@@ -20,19 +24,46 @@ const OtherTopNav = () => {
       </div>
       <div className="hidden lg:block">
         <ul className="flex items-center gap-3 *:text-nowrap">
-          <li className="after:contents-[''] relative flex cursor-pointer gap-2 capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full 2xl:gap-[13px]">
-            <span className="text-[22px]">
-              <TiGroupOutline />
-            </span>
-            <span className="text-sm">project area</span>
+          <li
+            className={cn(
+              "after:contents-[''] relative cursor-pointer capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full",
+              location.pathname === "/dashboard/projects"
+                ? "text-primary-color-600 after:w-full"
+                : "",
+            )}
+          >
+            <Link
+              to={"/dashboard/projects"}
+              className="flex gap-2 2xl:gap-[13px]"
+            >
+              <span className="text-[22px]">
+                <TiGroupOutline />
+              </span>
+              <span className="text-sm">project area</span>
+            </Link>
           </li>
-          <li className="after:contents-[''] relative flex cursor-pointer gap-2 capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full 2xl:gap-[13px]">
-            <span className="text-[22px]">
-              <LiaTrophySolid />
-            </span>
-            <span className="text-sm">get certificate</span>
+          <li
+            className={cn(
+              "after:contents-[''] relative cursor-pointer capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full",
+              location.pathname === "/dashboard/certificate"
+                ? "text-primary-color-600 after:w-full"
+                : "",
+            )}
+          >
+            <Link
+              to={"/dashboard/certificate"}
+              className="flex gap-2 2xl:gap-[13px]"
+            >
+              <span className="text-[22px]">
+                <LiaTrophySolid />
+              </span>
+              <span className="text-sm">get certificate</span>
+            </Link>
           </li>
-          <li className="after:contents-[''] relative flex cursor-pointer gap-2 capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full 2xl:gap-[13px]">
+          <li
+            className="after:contents-[''] relative flex cursor-pointer gap-2 capitalize text-tertiary-color-700 transition-colors duration-300 ease-linear after:absolute after:-bottom-2 after:left-0 after:block after:h-px after:w-0 after:bg-[#CC1747] hover:text-primary-color-600 hover:after:w-full 2xl:gap-[13px]"
+            onClick={handleModal}
+          >
             <span className="text-[22px]">
               <FaRegHeart />
             </span>
--- src/pages/auth/components/Modal.jsx
+++ src/pages/auth/components/Modal.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function Modal({ children }) {
   return (
-    <div className="absolute left-0 top-0 z-10 flex h-screen w-full items-center justify-center bg-black/25 px-6">
+    <div className="fixed left-0 top-0 z-10 flex h-screen w-full items-center justify-center bg-black/25 px-6">
       {children}
     </div>
   );
