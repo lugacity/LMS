efcc1300f114e0bf7ee9198453df232536dad950
kareem roqib <kareemolakunleh@gmail.com>
2024-08-15 18:53:28 +0100

    feat: add active class to the sections on click and add live session section

--- src/pages/dashboard/ShareDocument.jsx
+++ src/pages/dashboard/ShareDocument.jsx
@@ -1,47 +1,30 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
-import liveSession from "../../assets/images/dashboard/live-session.png";
+import { ScrollRestoration } from "react-router-dom";
+
 import CourseSection from "@/Components/dashboard/CourseSection";
-
-import DashboardSliderNav, {
-  MobileSlideNave,
-} from "@/Components/dashboard/DashboardSliderNav";
+import CourseVideoSection from "@/Components/dashboard/CourseVideoSection";
 import { useState } from "react";
-import MobileContent from "@/Components/dashboard/MobileContent";
-function ShareDocument() {
-  const [comp, setcomp] = useState("course sections");
+import LiveSession from "@/Components/dashboard/LiveSession";
 
+function ShareDocument() {
+  const [session, setSession] = useState("live");
+  const [sectionDetails, setSectionDetails] = useState({
+    section: "",
+    topic: "",
+  });
   return (
     <>
-      <div className="gap-4 bg-[#FDFDFD] lg:grid lg:grid-cols-[2.8fr_1fr]">
-        <section>
-          <div className="overflow-hidden rounded-[10px] lg:h-[400px] lg:w-[700px]">
-            <img
-              src={liveSession}
-              alt="live session"
-              width={897}
-              height={532}
-              className="object-cover"
-            />
-          </div>
-          <h1 className="my-6 text-2xl font-semibold leading-[28px] text-black *:block">
-            <span>Section 2</span>
-            <span> Introduction to Project Consulting Recordings </span>
-          </h1>
-          <div className="hidden lg:block">
-            <DashboardSliderNav />
-          </div>
-          <div className="lg:hidden">
-            <MobileSlideNave active={comp} setactive={setcomp} />
-          </div>
-          <div className="hidden lg:block">
-            <Outlet />
-          </div>
-          <div className="lg:hidden">
-            <MobileContent name={comp} />
-          </div>
-        </section>
+      <ScrollRestoration />
+      <div className="w-full gap-4 lg:grid lg:grid-cols-[2.8fr_1fr]">
+        {session === "live" && <LiveSession />}
+        {session === "recorded" && (
+          <CourseVideoSection sectionDetails={sectionDetails} />
+        )}
+
         <aside className="hidden rounded-[12px] border border-[#E4E7EC] bg-white px-4 py-6 lg:block">
-          <CourseSection />
+          <CourseSection
+            setSession={setSession}
+            setSectionDetails={setSectionDetails}
+          />
         </aside>
       </div>
     </>
