0895fcbe23c1575e9c4f5d5a52a9ed1d50254457
kareem roqib <kareemolakunleh@gmail.com>
2024-11-15 17:22:45 +0100

    fix: fix-bug

--- src/pages/admin-pages/course-management/CourseCohortsPreview.jsx
+++ src/pages/admin-pages/course-management/CourseCohortsPreview.jsx
@@ -11,7 +11,7 @@ import DashButton from "@/pages/auth/ButtonDash";
 import { useState } from "react";
 import { FaCheck } from "react-icons/fa6";
 
-import { Link, useParams, useSearchParams} from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 
 import { ClipLoader } from "react-spinners";
 // import AllCohorts from "./AllCohorts";
@@ -60,7 +60,7 @@ const CourseCohortsPreview = () => {
       data: {
         cohort,
       },
-      courseId
+      courseId,
     });
   };
 
@@ -71,8 +71,6 @@ const CourseCohortsPreview = () => {
     return new Intl.DateTimeFormat(locale).format(createdAt);
   };
 
-
-  
   const [queryString] = useSearchParams();
 
   return (
