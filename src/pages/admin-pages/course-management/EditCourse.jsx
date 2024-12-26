692092067f50052ccc6b06975fafe876d7d4a730
lugacity <yinkaabeebadesina@gmail.com>
2024-12-25 23:10:15 -0800

    worked on delete  course under course management

--- src/pages/admin-pages/course-management/EditCourse.jsx
+++ src/pages/admin-pages/course-management/EditCourse.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import CourseInformation from "@/Components/admindashboard/course-management/CourseInformation";
 import CourseCohortsPreview from "./CourseCohortsPreview";
 import OnDemand from "./OnDemand";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { CommonButton } from "@/Components/ui/button";
 import { useUnpublishCourse } from "@/hooks/course-management/use-unpublish-course";
+import { useDeleteCourse } from "@/hooks/course-management/use-delete-a-course";
 // import { IoSearch } from "react-icons/io5";
 // import BorderCard from "@/Components/BorderCard";
 // import Modal from "@/pages/auth/components/Modal";
@@ -14,7 +15,7 @@ import { useUnpublishCourse } from "@/hooks/course-management/use-unpublish-cour
 // import { Heading, Paragraph } from "@/pages/auth/components/Text";
 
 const EditCourse = () => {
-  // const [modal, setModal] = useState(false);
+  const navigate = useNavigate();
   const [activeSection, setActiveSection] = useState("courseInfo");
 
   const [queryString] = useSearchParams();
@@ -26,11 +27,10 @@ const EditCourse = () => {
   const { courseId } = useParams();
   // console.log("courseId", courseId);
 
+  const {courseDelete, isPending } = useDeleteCourse();
   const { unPublish, isUnPublishing } = useUnpublishCourse();
-  console.log("unPublish function:", unPublish);
-  console.log("isUnPublishing state:", isUnPublishing);
-
-
+  // console.log("unPublish function:", unPublish);
+  // console.log("isUnPublishing state:", isUnPublishing);
 
   const handleUnpublish = () => {
     if (!courseId) {
@@ -38,7 +38,15 @@ const EditCourse = () => {
       return;
     }
     unPublish({ courseId });
+    navigate("/admin/course/management");
+    // console.error("Course ID is unpublished.", {courseId});
   };
+
+
+  const handleDelete = () => {
+    courseDelete({ courseId });
+    console.log("Course Deteled Successfully", { courseId });
+  }
 
   return (
     <div>
@@ -95,12 +103,20 @@ const EditCourse = () => {
       </div>
 
       <div className="my-6 flex items-center justify-between gap-6">
-        <CommonButton onClick={handleUnpublish} disabled={isUnPublishing}  className="ml-auto border border-gray-500 bg-transparent text-gray-500 hover:bg-gray-300">
+        <CommonButton
+          onClick={handleUnpublish}
+          disabled={isUnPublishing}
+          className="ml-auto border border-gray-500 bg-transparent text-gray-500 hover:bg-gray-300"
+        >
           {isUnPublishing ? "Unpublishing..." : "Unpublish"}
         </CommonButton>
 
-        <CommonButton className="block bg-primary-color-600 font-normal">
-          Delete Course
+        <CommonButton
+          onClick={handleDelete}
+          disabled={isPending}
+          className="block bg-primary-color-600 font-normal"
+        >
+          {isPending ? "Delecting..." : "Delete Course"}
         </CommonButton>
       </div>
     </div>
